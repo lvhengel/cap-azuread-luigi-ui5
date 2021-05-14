5e3bb2dea6f10ae5f61962fa08a3afa7c984c81b
Leo van Hengel <leo.van.hengel@gmail.com>
2021-05-14 21:00:58 +0200

    add ui5 luigi app

---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const cds = require("@sap/cds");
+const helmet = require("helmet");
+const cors = require("cors");
+//const proxy = require("@sap/cds-odata-v2-adapter-proxy");
+
+cds.on("bootstrap", (app) => {
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+  app.use(cors());
+  //app.use(proxy());
+});
+
+module.exports = cds.server;
